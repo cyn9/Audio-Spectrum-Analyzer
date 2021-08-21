67f2687b8884fdb3fb68eeb6c162ae0ad3c23daf
cyn9 <cihan.asci@tufts.edu>
2021-08-21 18:06:44 +0300

    Added Copy button to the Help Window.

--- src/HelpWindow.py
+++ src/HelpWindow.py
@@ -1,5 +1,12 @@
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMessageBox
+from PyQt5.QtWidgets import QStyle
 from PyQt5 import uic
 from PyQt5 import QtWidgets
+
+from pyqtgraph.Qt import QtGui
+
+import pyperclip
 
 class HelpWindow(QtWidgets.QMainWindow):
     def __init__(self):
@@ -22,4 +29,36 @@ class HelpWindow(QtWidgets.QMainWindow):
         uic.loadUi('/Users/ciyan/Documents/Audio-Spectrum-Analyzer/ui/HelpWin.ui', self)
         self.setFixedSize(HELP_WIN_SCR_WIDTH, HELP_WIN_SCR_HEIGHT)
 
+        # Button onClick event for close
         self.btn_Close.clicked.connect(lambda: self.close())
+
+        # Button onClick event for copy
+        self.btn_Copy.clicked.connect(self.copy)
+
+
+    def copy(self):
+        """
+        Copies the text on the help window to the clipboard.
+
+        Parameters
+        ----------
+            None
+        
+        Returns
+        -------
+            None
+        """
+        helpText = self.txtHelp.toPlainText()
+
+        pyperclip.copy(helpText)
+
+        # "Copied" message info box:
+        copiedStyle = QApplication.style()
+        copiedText = 'Copied help text to clipboard.'
+        copiedMsg = QMessageBox()
+        copiedMsg.setWindowTitle("Hi!")
+        copiedMsg.setIcon(QMessageBox.Information)
+        copiedMsg.setText(copiedText)
+        copiedMsg.setWindowIcon(QtGui.QIcon(copiedStyle.standardIcon(QStyle.SP_DialogApplyButton)))
+        copiedMsg.exec_()
+
